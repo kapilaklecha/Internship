2ce5f9e29818a12a06507a88dd0b8307e4dd66ee
kapilaklecha <kapilaklecha@gmail.com>
2024-09-20 20:07:01 +0530

    wheel ui fixed

--- Wheel-of-names/wheel/wheel.js
+++ Wheel-of-names/wheel/wheel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const canvas = document.getElementById("wheelCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -52,19 +51,3 @@ function drawWheel() {
 }
 
 drawWheel();
-=======
-document.getElementById("canvas");
-
-function draw() {
-  const canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-
-    const circle = new Path2D();
-    circle.arc(100, 35, 25, 0, 2 * Math.PI);
-
-    ctx.fill(circle);
-  }
-}
-draw();
->>>>>>> parent of 0fd8d62... Added wheel ui
