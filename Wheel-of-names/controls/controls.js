6b401d5ed15c213344d1a43290947913afb5220c
kapilaklecha <kapilaklecha@gmail.com>
2024-09-23 19:14:10 +0530

    Added show and hide functionality to control panel

--- Wheel-of-names/controls/controls.js
+++ Wheel-of-names/controls/controls.js
@@ -5,6 +5,9 @@ const entriesTabBtn = document.querySelector("#entries-btn");
 const resultsTabBtn = document.querySelector("#results-btn");
 const resultsTab = document.querySelector("#results-wrapper");
 const entriesTab = document.querySelector("#controls-wrapper");
+const hideBtn = document.querySelector("#hide-btn");
+const controlParent = document.querySelector("#controls-board");
+const headerLeftC = document.querySelector(".controls-h-left");
 
 let userObj = {};
 
@@ -18,6 +21,7 @@ textArea.addEventListener("input", () => {
 function showTabEntries() {
   entriesTab.style.display = "block";
   resultsTab.style.display = "none";
+  console.log(hideBtn.checked);
 }
 
 function showTabResults() {
@@ -27,3 +31,17 @@ function showTabResults() {
 
 entriesTabBtn.addEventListener("click", showTabEntries);
 resultsTabBtn.addEventListener("click", showTabResults);
+
+function hideShow() {
+  if (hideBtn.checked) {
+    controlParent.style.display = "none";
+    headerLeftC.style.display = "none";
+  }
+
+  if (!hideBtn.checked) {
+    controlParent.style.display = "block";
+    headerLeftC.style.display = "block";
+  }
+}
+
+hideBtn.addEventListener("click", hideShow);
