954a39468d4135c9b49a92f0f18ee92e2e6ccbfd
kapilaklecha <kapilaklecha@gmail.com>
2024-09-23 15:08:41 +0530

    Added store

---
+++ Wheel-of-names/store.js
@@ -0,0 +1,6 @@
+// If you see any entries hard coded they are for testing purpose. To be removed.
+export let user = [
+  { text: "tom", color: "" },
+  { text: "sam", color: "" },
+  { text: "herry", color: "" },
+];
