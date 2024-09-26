39d42667e5b899ef252563d8ab2082d93b8e0b54
kapilaklecha <kapilaklecha@gmail.com>
2024-09-26 13:27:06 +0530

    Added resultsArray

--- Wheel-of-names/store.js
+++ Wheel-of-names/store.js
@@ -1,6 +1,3 @@
-// If you see any entries hard coded they are for testing purpose. To be removed.
-export let user = [
-  { text: "tom", color: "" },
-  { text: "sam", color: "" },
-  { text: "herry", color: "" },
-];
+export let users = [];
+
+export let resultsArr = [];
